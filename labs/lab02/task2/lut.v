ad450f0474d9dc11ec4ec97867409caa9a46f829
Mitali-Kabra7 <f20250701@pilani.bits-pilani.ac.in>
2026-09-19 13:35:13 +0000

    Fix task2 implementation and rebuild sim.vvp

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -1,15 +1,14 @@
 // lut.v
-// A small parameterized ROM (lookup table): DEPTH words, each WIDTH bits
-// wide. dout continuously reflects mem[sel].
-//
-// YOU complete the two TODOs below. Everything else is given.
+// A small parameterized ROM (lookup table): DEPTH words, each WIDTH bits wide.
+// dout continuously reflects mem[sel].
 
 module lut #(
   parameter WIDTH = 8,
-  parameter DEPTH = 4
+  parameter DEPTH = 4,
+  parameter SEL_WIDTH = $clog2(DEPTH)
 ) (
-  input      [$clog2(DEPTH)-1:0] sel,
-  output reg [WIDTH-1:0]         dout
+  input      [SEL_WIDTH-1:0] sel,
+  output reg [WIDTH-1:0]     dout
 );
 
   reg [WIDTH-1:0] mem [0:DEPTH-1];
@@ -17,12 +16,15 @@ module lut #(
   integer i;
 
   // TODO: initialize mem[i] = i*i for every i from 0 to DEPTH-1.
-  // Use an initial block with a for loop -- this is the only place a ROM's
-  // contents should be set up. (See the lab manual for why.)
-  
+  initial begin
+    for (i = 0; i < DEPTH; i = i + 1) begin
+      mem[i] = i * i;
+    end
+  end
 
-  // TODO: make dout continuously reflect mem[sel]. This is a combinational
-  // read -- pick the right procedural block and sensitivity list.
-
+  // TODO: make dout continuously reflect mem[sel].
+  always @(*) begin
+    dout = mem[sel];
+  end
 
 endmodule
